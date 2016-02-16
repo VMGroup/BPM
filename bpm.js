c7a576285301e49be346d096d07e081266c875ef
Pisces000221 <1786762946@qq.com>
2016-02-16 10:11:27 +0800

    Draw sticks

--- bpm.js
+++ bpm.js
@@ -18,12 +18,12 @@
     var history_ct = Math.ceil(w / 160) + 1;
     for (var i = 0; i < Math.min(history_ct, this.records.length - 1); ++i) {
       this.drawctx.beginPath();
-      this.drawctx.arc(
-        w - i * 160 - 80 + Math.max(0, Math.pow(1 - (Date.now() - this.last_pat) / 200, 3)) * 160 + 10,
-        h - h * (this.records[this.records.length - 1 - i] - this.records[this.records.length - 2 - i]) / 2000,
-        10, 0, 2 * Math.PI);
+      var x = w - i * 160 - 80 + Math.max(0, Math.pow(1 - (Date.now() - this.last_pat) / 200, 3)) * 160 + 10,
+          y = h - h * (this.records[this.records.length - 1 - i] - this.records[this.records.length - 2 - i]) / 2000;
+      this.drawctx.arc(x, y, 10, 0, 2 * Math.PI);
       this.drawctx.fillStyle = '#bbbb88';
       this.drawctx.fill();
+      this.drawctx.fillRect(x - 3.09, y, 6.18, h - y);
     }
     window.requestAnimationFrame(this.ticker);
   };
