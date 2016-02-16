b611a0c1698570b0e392bd66e774a898d45e230d
Pisces000221 <1786762946@qq.com>
2016-02-16 10:13:34 +0800

    Reduce redraw calls

--- bpm.js
+++ bpm.js
@@ -13,24 +13,26 @@
 
   bpm.refresh_display = function () {
     var w = this.canvas.clientWidth, h = this.canvas.clientHeight;
+    var dt = Date.now() - this.last_pat;
     this.drawctx.fillStyle = '#ffffcc';
     this.drawctx.fillRect(0, 0, w, h);
     var history_ct = Math.ceil(w / 160) + 1;
     for (var i = 0; i < Math.min(history_ct, this.records.length - 1); ++i) {
       this.drawctx.beginPath();
-      var x = w - i * 160 - 80 + Math.max(0, Math.pow(1 - (Date.now() - this.last_pat) / 200, 3)) * 160 + 10,
+      var x = w - i * 160 - 80 + Math.max(0, Math.pow(1 - dt / 200, 3)) * 160 + 10,
           y = h - h * (this.records[this.records.length - 1 - i] - this.records[this.records.length - 2 - i]) / 2000;
       this.drawctx.arc(x, y, 10, 0, 2 * Math.PI);
       this.drawctx.fillStyle = '#bbbb88';
       this.drawctx.fill();
       this.drawctx.fillRect(x - 3.09, y, 6.18, h - y);
     }
-    window.requestAnimationFrame(this.ticker);
+    if (dt < 200) window.requestAnimationFrame(this.ticker);
   };
 
   bpm.pat = function () {
     this.last_pat = Date.now();
     this.records.push(this.last_pat);
+    window.requestAnimationFrame(this.ticker);
   };
 
   bpm.create = function (id) {
