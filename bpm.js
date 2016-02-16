953a4e78f0ec68fb087cba056f4e732991cf878d
Pisces000221 <1786762946@qq.com>
2016-02-16 09:19:38 +0800

    Move dots horizontally

--- bpm.js
+++ bpm.js
@@ -15,13 +15,15 @@
     var w = this.canvas.clientWidth, h = this.canvas.clientHeight;
     this.drawctx.fillStyle = '#ffffcc';
     this.drawctx.fillRect(0, 0, w, h);
-    // For testing
-    this.drawctx.beginPath();
-    this.drawctx.arc(
-      w / 2, Math.max(0, Math.pow(1 - (Date.now() - this.last_pat) / 200, 3)) * h + 10,
-      10, 0, 2 * Math.PI);
-    this.drawctx.fillStyle = '#bbbb88';
-    this.drawctx.fill();
+    var history_ct = Math.ceil(w / 160) + 2;
+    for (var i = 0; i < history_ct; ++i) {
+      this.drawctx.beginPath();
+      this.drawctx.arc(
+        w - i * 160 + 80 + Math.max(0, Math.pow(1 - (Date.now() - this.last_pat) / 200, 3)) * 160 + 10,
+        h / 2, 10, 0, 2 * Math.PI);
+      this.drawctx.fillStyle = '#bbbb88';
+      this.drawctx.fill();
+    }
     window.requestAnimationFrame(this.ticker);
   };
 
@@ -42,11 +44,14 @@
     bpm.init_display(canvas);
 	
     var ret = {};
+    // Properties
     ret.canvas = canvas;
     ret.drawctx = canvas.getContext('2d');
     ret.last_pat = Date.now();
+    // Methods
     ret.refresh_display = bpm.refresh_display;
     ret.pat = bpm.pat;
+    // Timers
     ret.ticker = (function (_ret) { return function () { _ret.refresh_display(); }; })(ret);
     window.requestAnimationFrame(ret.ticker);
     return ret;
