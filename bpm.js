4cf3151aef121ce2c3c033b44e9a9d1e35d1c45a
Pisces000221 <1786762946@qq.com>
2016-02-16 10:54:21 +0800

    Minor UI & algorithm adjustments

--- bpm.js
+++ bpm.js
@@ -34,18 +34,18 @@
     for (var i = 0; i < Math.min(history_ct, this.records.length - 1); ++i) {
       this.drawctx.beginPath();
       var x = w - i * 160 - 80 + Math.max(0, Math.pow(1 - dt / 200, 3)) * 160 + 10,
-          y = h - h * (this.records[this.records.length - 1 - i] - this.records[this.records.length - 2 - i]) / 2000;
+          y = h - h * (this.records[this.records.length - 1 - i] - this.records[this.records.length - 2 - i]) / 1500;
       this.drawctx.arc(x, y, 10, 0, 2 * Math.PI);
       this.drawctx.fill();
       this.drawctx.fillRect(x - 3.09, y, 6.18, h - y);
     }
     // Estimation
     this.drawctx.fillStyle = '#999955';
-    this.drawctx.font = '28px Droid Sans Mono, Source Code Pro, Menlo, Courier New, Monospace';
+    this.drawctx.font = '34px Droid Sans Mono, Source Code Pro, Menlo, Courier New, Monospace';
     this.drawctx.textBaseline = 'bottom';
     var text_size = this.drawctx.measureText('Est.');
     this.drawctx.fillText('Est.', w - text_size.width - 6, h * 0.5);
-    this.drawctx.font = '54px Droid Sans Mono, Source Code Pro, Menlo, Courier New, Monospace';
+    this.drawctx.font = '64px Droid Sans Mono, Source Code Pro, Menlo, Courier New, Monospace';
     var estimation_str = '---';
     this.drawctx.textBaseline = 'top';
     if (this.records.length > 8) {
@@ -55,9 +55,9 @@
       var intv_list = [0, 0, 0, 0, 0, 0, 0, 0];
       for (var i = 0; i < 8; ++i)
         intv_list[i] = this.records[this.records.length - 1 - i] - this.records[this.records.length - 2 - i];
-      intv_list.sort();
+      intv_list.sort(function (a, b) { return a - b; });
       // (3) Ignore cases where the beats are unstable
-      if (intv_list[7] - intv_list[0] <= intv_list[0] * 0.5) {
+      if (intv_list[7] - intv_list[0] <= intv_list[7] * 0.5) {
         var e1 = 60000.0 / ((intv_list[3] + intv_list[4]) / 2);
         var e2 = 60000.0 / ((this.records[this.records.length - 1] - this.records[this.records.length - 9]) / 9);
         var estimation = (e1 + e2) / 2;
